88d277239cbeb3b759f58b3a1193a1ac7604e252
teo <teodublin@gmail.com>
2025-01-14 14:30:34 +0100

    inp

--- includes/class.php
+++ includes/class.php
@@ -1,15 +1,6 @@
 <?php
-    function Html(): Html{
-        return new Html();
-    }
     function Sql(): Sql{
         return new Sql();
-    }
-    function Session():Session{
-        return new Session();
-    }
-    function Template($key): Template{
-        return new Template($key);
     }
     function Select(string $select):Select{
         return new Select($select);
@@ -23,12 +14,6 @@
     function Enum(string $table,string $column):Enum{
         return new Enum($table,$column);
     }
-    /**
-     * Creates a new Delete object with the given ID.
-     *
-     * @param string $id The ID of the record to delete.
-     * @return Delete The Delete object for chaining.
-     */
     function Delete(string $id):Delete{
         return new Delete($id);
     }

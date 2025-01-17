a6ab4b2bcf0a9447d8eda6d905e3430d21f54b71
teo <teodublin@gmail.com>
2025-01-17 17:48:37 +0100

    inp

--- end_points/planning.php
+++ end_points/planning.php
@@ -1,4 +1,8 @@
 <?php
 include '../functions.php';
 include '../class/select.php';
-echo "test2";
+$_REQUEST['id_terapista']=11;
+$_REQUEST['data']='2024-12-30';
+$busy=$ret=[];
+$planning=(new Select('*'))->from('planning')->where("id_terapista = {$_REQUEST['id_terapista']} AND data='{$_REQUEST['data']}'")->get();
+echo "test3";

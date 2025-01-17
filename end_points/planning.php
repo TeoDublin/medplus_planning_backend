7910012ca9cdcf71a03ceaff766c2b719e6b2af6
teo <teodublin@gmail.com>
2025-01-17 18:27:13 +0100

    inp

--- end_points/planning.php
+++ end_points/planning.php
@@ -3,7 +3,7 @@ include '../functions.php';
 include '../class/select.php';
 $_REQUEST['id_terapista']=11;
 $_REQUEST['data']='2024-12-30';
-echo json_encode((new Select('p.id,p.motivo as reason, p.origin, pr.ora as start, pe.ora as end'))
+echo json_encode((object)(new Select('p.id,p.motivo as reason, p.origin, pr.ora as start, pe.ora as end'))
     ->from('planning','p')
     ->left_join('planning_row pr ON p.row_inizio = pr.ID')
     ->left_join('planning_row pe ON p.row_fine = pe.ID')

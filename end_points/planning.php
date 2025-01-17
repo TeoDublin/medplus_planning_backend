614ea8f40c39067e0c692dd75f77674ffd365d49
teo <teodublin@gmail.com>
2025-01-17 17:48:57 +0100

    inp

--- end_points/planning.php
+++ end_points/planning.php
@@ -5,4 +5,5 @@ $_REQUEST['id_terapista']=11;
 $_REQUEST['data']='2024-12-30';
 $busy=$ret=[];
 $planning=(new Select('*'))->from('planning')->where("id_terapista = {$_REQUEST['id_terapista']} AND data='{$_REQUEST['data']}'")->get();
-echo "test3";
+foreach($planning as $plan)for($i=$plan['row_inizio'];$i<=$plan['row_fine'];$i++)$busy[$i]=$plan;
+echo "test4";

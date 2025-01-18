72dfccfcad791e2e3e6851de2759ba5f1e27e70b
teo <teodublin@gmail.com>
2025-01-18 19:47:50 +0100

    inp

--- end_points/planning.php
+++ end_points/planning.php
@@ -4,47 +4,34 @@ include '../class/select.php';
 $_REQUEST['id_terapista']=11;
 $_REQUEST['data']='2025-01-18';
 $busy=$ret=[];
-$is_first=true;
+$skip=0;
 function busy_row($busy_row){
-    if(!$busy_row)return ['id'=>'-','origin'=>'-','motivo'=>'-'];
+    if(!$busy_row)return ['id'=>'-','motivo'=>'LIBERO'];
     else return [
         'id'=>"{$busy_row['origin']}_{$busy_row['id']}",
-        'origin'=>$busy_row['origin'] ?? '-',
         'reason'=>$busy_row['motivo'] ?? '-'
     ];
 }
+$planning_row=(new Select('id,DATE_FORMAT(ora,\'%H:%i\') as hour'))->from('planning_row')->get();
 $planning=(new Select('*'))->from('planning')->where("id_terapista = {$_REQUEST['id_terapista']} AND data='{$_REQUEST['data']}'")->get();
 foreach($planning as $plan)for($i=$plan['row_inizio'];$i<=$plan['row_fine'];$i++)$busy[$i]=$plan;
-foreach((new Select('id,DATE_FORMAT(ora,\'%H:%i\') as hour'))->from('planning_row')->get() as $row){
-    $busy_row=busy_row($busy[$row['id']]);
-    if($busy_row['id']=="-"){
-        $ret[]=[
-            'id'=>$row['id'],
-            'related_id'=>$busy_row['id'],
-            'hour'=>$row['hour'],
-            'origin'=>$busy_row['origin'],
-            'reason'=>$busy_row['reason'],
-        ];
-    }
-    elseif($is_first){
-        $row_span=0;$related_id=$busy_row['id'];(int)$row_id=$row['id'];
+foreach( $planning_row as $row){
+    if($skip==0){
+        $busy_row=busy_row($busy[$row['id']]);
+        $related_id=$busy_row['id'];
         while ($related_id==$busy_row['id']) {
             $related_id=busy_row($busy[++$row_id])['id'];
-            $row_span++;
+            $skip++;
         }
+        $id = (int)$row['id']+$skip;
         $ret[]=[
-            'id'=>$row['id'],
-            'related_id'=>$busy_row['id'],
-            'hour'=>$row['hour'],
+            'id'=>$busy_row['id'],
+            'hour'=>"{$row['hour']} - {$planning_row[$id]['hour']}",
             'origin'=>$busy_row['origin'],
             'reason'=>$busy_row['reason'],
             'row_span'=>$row_span
         ];
-        $is_first=false;
     }
-    else {
-        $ret[]=['id'=>$row['id'],'hour'=>$row['hour']];
-        if(--$row_span==0)$is_first=true;
-    }
+    else --$skip;
 }
 echo json_encode((object)['planning_row'=>$ret]);

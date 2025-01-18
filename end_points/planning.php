4076d8487b87d05b0f432c666d4abee64372e67b
teo <teodublin@gmail.com>
2025-01-18 16:56:44 +0100

    inp

--- end_points/planning.php
+++ end_points/planning.php
@@ -2,11 +2,11 @@
 include '../functions.php';
 include '../class/select.php';
 $_REQUEST['id_terapista']=11;
-$_REQUEST['data']='2024-01-18';
+$_REQUEST['data']='2025-01-18';
 $busy=$ret=[];
 $planning=(new Select('*'))->from('planning')->where("id_terapista = {$_REQUEST['id_terapista']} AND data='{$_REQUEST['data']}'")->get();
 foreach($planning as $plan)for($i=$plan['row_inizio'];$i<=$plan['row_fine'];$i++)$busy[$i]=$plan;
-foreach((new Select('*'))->from('planning_row')->get() as $row){
+foreach((new Select('id,DATE_FORMAT(ora,\'%h:%m\') as ora'))->from('planning_row')->get() as $row){
     $ret[$row['id']]=[
         'id'=>$row['id'],
         'hour'=>$row['ora'],
